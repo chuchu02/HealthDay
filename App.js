62cb6a42d2ed5a793a69de4786a99fd19e9edd66
chuchu02 <rlaxogns73@gmail.com>
2022-11-07 11:03:34 +0900

    RN 기초세팅

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import tw from 'twrnc';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={tw`text-3xl text-red-500`}>1111</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
